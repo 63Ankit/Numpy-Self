8714dd4d6aeb73d972304368dc70ff58ccb5ef3f
Ankit <Ankit6391346034@gmail.com>
2025-09-28 23:26:00 +0530

    Commit 1

---
+++ q1.py
@@ -0,0 +1,16 @@
+'''
+import numpy as np
+
+a = np.array([10, 20, 30])
+print("Array:", a)
+print("Mean:", np.mean(a))
+print("Standard Deviation:", np.std(a))
+'''
+import numpy as np
+
+arr = np.array([1, 2, 3, 4, 5])
+
+print(arr)
+
+
+print(type(arr))
